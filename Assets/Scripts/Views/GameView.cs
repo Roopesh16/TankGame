4da19051c8d520998a189a50b46407e5a8997a0d
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-07-06 18:35:50 +0530

    Add Game View

---
+++ Assets/Scripts/Views/GameView.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame.Views
+{
+    public class GameView : MonoBehaviour
+    {
+        #region ------------ Serialize Variables ------------
+        #endregion------------------------
+
+        #region ------------ Private Variables ------------
+        #endregion------------------------
+
+        #region ------------ Public Variables ------------
+        #endregion------------------------
+
+        #region ------------ Monobehavior Methods ------------
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion------------------------
+
+        #region ------------ Public Methods ------------
+        #endregion------------------------
+
+        #region ------------ Private Methods ------------
+        #endregion------------------------
+    }
+}
