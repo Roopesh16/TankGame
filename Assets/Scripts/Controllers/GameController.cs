7aac1455851d0e128be7102196f9c045e39fb72c
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-07-06 18:34:23 +0530

    Add Game Controller

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TankGame.Managers;
+using TankGame.Models;
+using UnityEngine;
+
+namespace TankGame.Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+        #region ------------ Serialize Variables ------------
+        [SerializeField] private TankController tankController;
+        #endregion------------------------
+
+        #region ------------ Private Variables ------------
+        #endregion------------------------
+
+        #region ------------ Public Variables ------------
+        #endregion------------------------
+
+        #region ------------ Monobehavior Methods ------------
+        void Start()
+        {
+            OnGameStart();
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion------------------------
+
+        #region ------------ Public Methods ------------
+        #endregion------------------------
+
+        #region ------------ Private Methods ------------
+        private void OnGameStart()
+        {
+            GameManager.instance.SetGameState(GameState.PLAY);
+            tankController.OnGameStart();
+        }
+        #endregion------------------------
+    }
+}
