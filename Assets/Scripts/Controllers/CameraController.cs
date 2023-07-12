1aa02a3b4c4f2c95688991673876ca91a2e210fe
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-07-12 17:34:53 +0530

    Add Camera Controller

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TankGame.Managers;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform tank;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (GameManager.instance.GetTankState() == TankGame.Models.TankState.MOVING)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, tank.transform.position.z);
+        }
+    }
+}
