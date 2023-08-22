e712b9f6bd062bc55ba75f87db936b919e81ca83
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-08-22 22:52:03 +0530

    Add Audio Manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,32 +1,85 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace TankGame.Managers
 {
+    public enum AudioSFX
+    {
+        TANK_SHOT,
+        WALL_BREAK,
+        MINE_EXPLODE
+    }
+
+    public enum AudioBGM
+    {
+        TANK_IDLE,
+        TANK_MOVE,
+    }
+
     public class AudioManager : MonoBehaviour
     {
         #region ------------ Serialize Variables ------------
+        [SerializeField]
+        private List<AudioClip> sfxList = new List<AudioClip>();
+        [SerializeField]
+        private List<AudioClip> bgmList = new List<AudioClip>();
         #endregion------------------------
 
         #region ------------ Private Variables ------------
+        private bool isSfxMute = false;
+        private AudioSource sfxSource;
+        private AudioSource bgmSource;
         #endregion------------------------
 
         #region ------------ Public Variables ------------
+        public static AudioManager instance = null;
         #endregion------------------------
 
         #region ------------ Monobehavior Methods ------------
-        void Start()
+        void Awake()
         {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(this);
         }
 
-        void Update()
+        private void Start()
         {
+            sfxSource = gameObject.AddComponent<AudioSource>();
+            sfxSource.loop = false;
+            sfxSource.playOnAwake = false;
+            bgmSource = gameObject.AddComponent<AudioSource>();
+            bgmSource.loop = true;
+            bgmSource.playOnAwake = true;
 
         }
         #endregion------------------------
 
         #region ------------ Public Methods ------------
+        public void PlaySFX(AudioSFX sfxClip, float volume = 1f)
+        {
+            int sfxPosition = (int)sfxClip;
+            if (sfxList[sfxPosition] == null) return;
+
+            sfxSource.volume = volume;
+            sfxSource.PlayOneShot(sfxList[sfxPosition]);
+        }
+
+        public void PlayBGM(AudioBGM bgmClip, float volume = 1f)
+        {
+            int bgmPosition = (int)bgmClip;
+            if (bgmList[bgmPosition] == null) return;
+
+            bgmSource.clip = bgmList[bgmPosition];
+            bgmSource.volume = volume;
+            bgmSource.Play();
+        }
         #endregion------------------------
 
         #region ------------ Private Methods ------------
