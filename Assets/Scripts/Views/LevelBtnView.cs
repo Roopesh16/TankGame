2c49097d1b03dfbbd47f4ed0a34bd81903a3e689
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-09-09 15:48:55 +0530

    Unlock Level

---
+++ Assets/Scripts/Views/LevelBtnView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TankGame.Managers;
+
+namespace TankGame.Views
+{
+    public class LevelBtnView : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            LevelManager.instance.LoadScene(sceneName);
+        }
+    }
+}
