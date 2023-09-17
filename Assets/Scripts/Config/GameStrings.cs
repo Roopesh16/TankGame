dc85dece5afcb63b3de2521b78b1f27e56d81954
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-09-17 11:47:16 +0530

    Add Other strings

--- Assets/Scripts/Config/GameStrings.cs
+++ Assets/Scripts/Config/GameStrings.cs
@@ -1,4 +1,7 @@
 public static class GameStrings
 {
     public const string bulletString = "Bullet";
+    public const string tankString = "Tank";
+    public const string baseString = "Base";
+    public const string menuSceneString = "MenuScene";
 }
