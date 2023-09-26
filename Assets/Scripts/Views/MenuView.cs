ea6ac3de2cdcbbf0bf05a3d1c2ce82f23c916bb1
Roopesh Verma <40837868+Roopesh16@users.noreply.github.com>
2023-09-26 17:51:56 +0530

    Switch b/w info and title

--- Assets/Scripts/Views/MenuView.cs
+++ Assets/Scripts/Views/MenuView.cs
@@ -8,10 +8,14 @@ namespace TankGame.Views
     public class MenuView : MonoBehaviour
     {
         #region ------------ Serialize Variables ------------
-        [Header("UI")]
+        [Header("UI Screen")]
         [SerializeField] private GameObject titleScreen;
         [SerializeField] private GameObject menuScreen;
+        [SerializeField] private GameObject infoScreen;
+        [Header("UI Button")]
         [SerializeField] private Button playButton;
+        [SerializeField] private Button infoButton;
+        [SerializeField] private Button infoBackBtn;
         [SerializeField] private List<Button> levelButtons = new List<Button>();
 
         #endregion------------------------
@@ -24,6 +28,9 @@ namespace TankGame.Views
         private void Awake()
         {
             playButton.onClick.AddListener(PlayButton);
+            infoButton.onClick.AddListener(InfoButton);
+            infoBackBtn.onClick.AddListener(InfoBackButton);
+
             foreach (Button level in levelButtons)
             {
                 level.interactable = false;
@@ -44,6 +51,18 @@ namespace TankGame.Views
         {
             titleScreen.SetActive(false);
             menuScreen.SetActive(true);
+        }
+
+        private void InfoButton()
+        {
+            titleScreen.SetActive(false);
+            infoScreen.SetActive(true);
+        }
+
+        private void InfoBackButton()
+        {
+            titleScreen.SetActive(true);
+            infoScreen.SetActive(false);
         }
 
         public void UnlockLevel()
