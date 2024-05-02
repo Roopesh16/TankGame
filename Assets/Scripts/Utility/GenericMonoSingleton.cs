9a3560ca1d56600c2e08c6b092d2a722d1bcddc2
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-02 13:13:34 +0530

    Add generic mono singleton

---
+++ Assets/Scripts/Utility/GenericMonoSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TankGame.Utility
+{
+    public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+    {
+        private static T instance = null;
+
+        public static T Instance { get { return instance; } }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = (T)this;
+            }
+            else if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(this);
+        }
+    }
+}
