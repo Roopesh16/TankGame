49d85ffe887dbabb5470cb6ec66e9f07c5e84b85
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-03 19:04:54 +0530

    Set tank model

--- Assets/Scripts/Tank/TankModel.cs
+++ Assets/Scripts/Tank/TankModel.cs
@@ -2,6 +2,13 @@ namespace TankGame.Tank
 {
     public class TankModel
     {
+        private TankController tankController;
 
+        public float MaxDistance { get; private set; } = 5f;
+        public float MovementDuration { get; private set; } = 2f;
+        public float TankVelocity { get; private set; } = 3f;
+        public float MinimumDistance { get; private set; } = 2f;
+
+        public void SetController(TankController tankController) => this.tankController = tankController;
     }
 }
