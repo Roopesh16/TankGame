72d6f3e2c68d4d0ea59d78f2d61b0a495526df6a
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-04 12:38:33 +0530

    Add Input Service

---
+++ Assets/Scripts/Input/InputService.cs
@@ -0,0 +1,40 @@
+﻿using TankGame.Main;
+using TankGame.Managers;
+using TankGame.Models;
+using TankGame.Tank;
+using UnityEngine;
+
+namespace TankGame.Inputs
+{
+    public class InputService
+    {
+        private TankService tankService => GameService.Instance.TankService;
+        private RaycastHit rayHit;
+        private float maxHitDistance = 5f;
+
+        public InputService(float maxHitDistance)
+        {
+            this.maxHitDistance = maxHitDistance;
+        }
+
+        public void Update()
+        {
+            if (GameManager.instance.GetGameState() == GameState.PLAY)
+            {
+                if (Input.GetMouseButtonDown(0) && tankService.GetTankState() == TankState.REST)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    if (Physics.Raycast(ray, out rayHit, maxHitDistance))
+                    {
+                        if (rayHit.collider.tag == GameStrings.baseString)
+                        {
+                            tankService.SetTankState(TankState.MOVING);
+                            tankService.MoveTank();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
