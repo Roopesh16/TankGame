2de9d67115e1562b715ee9500a207a952b6da9fc
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-04 13:56:41 +0530

    Setup up tank controller

--- Assets/Scripts/Tank/TankController.cs
+++ Assets/Scripts/Tank/TankController.cs
@@ -1,62 +1,48 @@
 using TankGame.Main;
-using TankGame.Managers;
-using TankGame.Models;
 using UnityEngine;
 
 namespace TankGame.Tank
 {
-    public class TankController : MonoBehaviour
+    public class TankController
     {
         #region ------------ Serialize Variables ------------
-        [Header("References")]
-        [SerializeField] private TankView tankView;
-
-        [Header("Fields")]
-        [SerializeField] private float maxDistance = 5f;
+        private TankView tankView;
+        private TankModel tankModel;
 
         #endregion------------------------
 
         #region ------------ Private Variables ------------
         private TankService tankService => GameService.Instance.TankService;
-        private RaycastHit rayHit;
+
         #endregion------------------------
 
-        #region ------------ Monobehavior Methods ------------
-        void Update()
+        #region ------------ Private Methods ------------
+        private void InstantiateView(TankView tankView)
         {
-            if (GameManager.instance.GetGameState() == GameState.PLAY)
-            {
-                if (Input.GetMouseButtonDown(0) && tankService.GetTankState() == TankState.REST)
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            this.tankView = tankView;
+            tankView.SetController(this);
+            tankView.SetupTankView();
+        }
 
-                    if (Physics.Raycast(ray, out rayHit, maxDistance))
-                    {
-                        if (rayHit.collider.tag == GameStrings.baseString)
-                        {
-                            tankService.SetTankState(TankState.MOVING);
-                            tankView.MoveTank(rayHit.point);
-                        }
-                    }
-                }
-            }
+        private void InstantiateModel()
+        {
+            tankModel = new TankModel();
+            tankModel.SetController(this);
         }
         #endregion------------------------
 
         #region ------------ Public Methods ------------
         public TankController(TankView tankView)
         {
-            this.tankView = tankView;
-            tankModel = new TankModel();
-            tankModel.SetController(this);
-            tankView.SetController(this);
+            InstantiateView(tankView);
+            InstantiateModel();
         }
 
-        public void OnGameStart()
-        {
-            tankService.SetTankState(TankState.REST);
-            tankView.OnGameStart();
-        }
+        public float GetMinDistance() => tankModel.MinimumDistance;
+
+        public void SetTankState(TankState tankState) => tankService.SetTankState(tankState);
+
+        public void MoveTank(Vector3 hitPosition) => tankView.MoveTank(hitPosition);
         #endregion------------------------
     }
 }
