19a6400977d552f761834f418025b87d85f17357
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 14:27:56 +0530

    Add Event Controller

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TankGame.Events
+{
+    public class EventController
+    {
+        private event Action baseEvent;
+        public void Invoke() => baseEvent.Invoke();
+        public void AddListener(Action listener) => baseEvent += listener;
+        public void RemoveListener(Action listener) => baseEvent -= listener;
+    }
+}
