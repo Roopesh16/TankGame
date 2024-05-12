985aae59074094b9bd7d8afa362c4683c44a8809
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 20:11:35 +0530

    Set game start as generic event

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -2,12 +2,12 @@
 {
     public class EventService
     {
-        public EventController OnGameStart { get; private set; }
+        public EventController<bool> OnGameStart { get; private set; }
         public EventController OnGameOver { get; private set; }
 
         public EventService()
         {
-            OnGameStart = new EventController();
+            OnGameStart = new EventController<bool>();
             OnGameOver = new EventController();
         }
     }
