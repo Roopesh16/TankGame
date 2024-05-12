596183b0b2858a32c3ba45df5c2eb0e58a4681f0
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 20:34:19 +0530

    Add Level complete event

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -4,11 +4,13 @@
     {
         public EventController<bool> OnLevelStart { get; private set; }
         public EventController OnGameOver { get; private set; }
+        public EventController OnLevelComplete { get; private set; }
 
         public EventService()
         {
             OnLevelStart = new EventController<bool>();
             OnGameOver = new EventController();
+            OnLevelComplete = new EventController();
         }
     }
 }
