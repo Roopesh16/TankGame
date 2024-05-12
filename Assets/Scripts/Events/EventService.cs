1f2f7a901a75e05ffc738b7fcffc19184bd92e74
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 14:28:10 +0530

    Add Event Service

---
+++ Assets/Scripts/Events/EventService.cs
@@ -0,0 +1,14 @@
+﻿namespace TankGame.Events
+{
+    public class EventService
+    {
+        public EventController OnGameStart { get; private set; }
+        public EventController OnGameOver { get; private set; }
+
+        public EventService()
+        {
+            OnGameStart = new EventController();
+            OnGameOver = new EventController();
+        }
+    }
+}
