c9d7f9636de6abd2d55f1087045e5da550a7b833
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 14:28:42 +0530

    Add null check before invoke

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -5,7 +5,7 @@ namespace TankGame.Events
     public class EventController
     {
         private event Action baseEvent;
-        public void Invoke() => baseEvent.Invoke();
+        public void Invoke() => baseEvent?.Invoke();
         public void AddListener(Action listener) => baseEvent += listener;
         public void RemoveListener(Action listener) => baseEvent -= listener;
     }
