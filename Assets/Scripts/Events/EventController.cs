0c18d7a31188099051db75ad02eb581703ec0a0f
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-12 20:11:18 +0530

    Add generic event for 1 arg

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -9,4 +9,12 @@ namespace TankGame.Events
         public void AddListener(Action listener) => baseEvent += listener;
         public void RemoveListener(Action listener) => baseEvent -= listener;
     }
+
+    public class EventController<T>
+    {
+        private event Action<T> baseEvent;
+        public void Invoke(T type) => baseEvent?.Invoke(type);
+        public void AddListener(Action<T> listener) => baseEvent += listener;
+        public void RemoveListener(Action<T> listener) => baseEvent -= listener;
+    }
 }
