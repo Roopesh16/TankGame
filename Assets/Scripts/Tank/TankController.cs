115315edf990086543708e991779c77252fe0726
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-13 16:28:14 +0530

    Move tank and set position

--- Assets/Scripts/Tank/TankController.cs
+++ Assets/Scripts/Tank/TankController.cs
@@ -1,5 +1,7 @@
 using TankGame.Audio;
 using TankGame.Main;
+using TankGame.Managers;
+using TankGame.UI;
 using UnityEngine;
 
 namespace TankGame.Tank
@@ -9,12 +11,13 @@ namespace TankGame.Tank
         #region ------------ Serialize Variables ------------
         private TankView tankView;
         private TankModel tankModel;
-
+        private GunView gunView;
         #endregion------------------------
 
         #region ------------ Private Variables ------------
         private TankService tankService => GameService.Instance.TankService;
         private AudioService audioService => GameService.Instance.AudioService;
+        private UIService uIService => GameService.Instance.UIService;
 
         #endregion------------------------
 
@@ -34,23 +37,48 @@ namespace TankGame.Tank
         #endregion------------------------
 
         #region ------------ Public Methods ------------
-        public TankController(TankView tankView)
+        public TankController(TankView tankView, GunView gunView)
         {
+            this.gunView = gunView;
             InstantiateView(tankView);
             InstantiateModel();
         }
 
-        public float GetMinDistance() => tankModel.MinimumDistance;
-
         public void SetTankState(TankState tankState) => tankService.SetTankState(tankState);
 
-        public void MoveTank(Vector3 hitPosition) => tankView.MoveTank(hitPosition);
-
-        public void OnGameOver()
+        public void SetTankPosition(Vector3 hitPosition)
         {
-            audioService.SetBGMMute();
-            //gameController.OnGameOver();
+            tankView.Agent.isStopped = false;
+            tankView.ToggleMove(true);
+            tankView.SetHitPosition(hitPosition);
+            tankView.Animator.SetBool("IsMove", true);
+            SetAudio(AudioBGM.TANK_MOVE, 0.15f);
         }
+
+        public void MoveTank()
+        {
+            tankView.Agent.SetDestination(tankView.HitPosition);
+
+            if (Vector3.Distance(tankView.HitPosition, tankView.transform.position) <= tankModel.MinimumDistance)
+            {
+                tankView.Agent.isStopped = true;
+                tankView.ToggleMove(false);
+
+                if (gunView.IsWallPresent())
+                {
+                    SetTankState(TankState.FIRE);
+                    gunView.FireBullet();
+                }
+
+                tankView.Animator.SetBool("IsMove", false);
+                SetAudio(AudioBGM.TANK_IDLE, 0.8f);
+                SetTankState(TankState.REST);
+            }
+
+            tankView.transform.RotateAround(tankView.transform.position, tankView.transform.up, tankView.RotationSpeed * Time.deltaTime);
+        }
+
+        public void OnGameOver() => GameManager.Instance.OnGameOver();
 
         public void SetAudio(AudioBGM audioBGM, float volume = 0.5f) => audioService.PlayBGM(audioBGM, volume);
         #endregion------------------------
