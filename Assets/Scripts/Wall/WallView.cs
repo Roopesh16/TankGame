78f5316adc9918e03ff22270f307163849f790c6
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-13 17:07:22 +0530

    Refactor

--- Assets/Scripts/Wall/WallView.cs
+++ Assets/Scripts/Wall/WallView.cs
@@ -12,23 +12,24 @@ namespace TankGame.Wall
     {
         #region ------------ Serialize Variables ------------
         [SerializeField] private WallType wallType;
-        [SerializeField] private WallModel wallModel;
-        [SerializeField] private WallController wallController;
-        [SerializeField] private MoveCamController moveCamController;
+        
         #endregion------------------------
 
         #region ------------ Private Variables ------------
+        private WallController wallController;
         private int wallScore;
         private AudioService audioService => GameService.Instance.AudioService;
         private UIService uIService => GameService.Instance.UIService;
         private LevelService levelService => GameService.Instance.LevelService;
+
+        public int WallScore => wallScore;
+        public WallType WallType => wallType;
         #endregion------------------------
 
         #region ------------ Public Methods ------------
-        public void OnGameStart()
-        {
-            SetWallScore();
-        }
+        public void OnGameStart() => SetWallScore();
+
+        public void SetWallController(WallController wallController) => this.wallController = wallController;
         #endregion------------------------
 
         #region ------------ Private Methods ------------
