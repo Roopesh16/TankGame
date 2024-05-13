542406867c3c0c12f076a50f04f3657b9e643cfc
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-13 20:39:32 +0530

    Set level number

--- Assets/Scripts/Levels/LevelBtnView.cs
+++ Assets/Scripts/Levels/LevelBtnView.cs
@@ -8,13 +8,14 @@ namespace TankGame.Levels
     public class LevelBtnView : MonoBehaviour
     {
         [SerializeField] private Button levelButton;
+        [SerializeField] private int levelNumber;
         private LevelService levelService => GameService.Instance.LevelService;
 
         public void ToggleInteraction(bool isInteractable) => levelButton.interactable = isInteractable;
 
         public void LoadScene(string sceneName)
         {
-            levelService.SetLevel(sceneName[sceneName.Length - 1]);
+            levelService.SetLevel(levelNumber);
             GameManager.Instance.SetCurrentScene(sceneName);
             levelService.LoadScene(sceneName);
         }
