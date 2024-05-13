a7f615109150414fb58b23b28958cf84a5694284
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-13 17:32:47 +0530

    Set wall controller and set wall point

--- Assets/Scripts/Wall/WallView.cs
+++ Assets/Scripts/Wall/WallView.cs
@@ -1,8 +1,6 @@
 using TankGame.Audio;
-using TankGame.Cameras;
 using TankGame.Levels;
 using TankGame.Main;
-using TankGame.Models;
 using TankGame.UI;
 using UnityEngine;
 
@@ -12,50 +10,30 @@ namespace TankGame.Wall
     {
         #region ------------ Serialize Variables ------------
         [SerializeField] private WallType wallType;
-        
+
         #endregion------------------------
 
         #region ------------ Private Variables ------------
         private WallController wallController;
-        private int wallScore;
+        private int wallPoint;
         private AudioService audioService => GameService.Instance.AudioService;
         private UIService uIService => GameService.Instance.UIService;
         private LevelService levelService => GameService.Instance.LevelService;
 
-        public int WallScore => wallScore;
+        public int WallPoint => wallPoint;
         public WallType WallType => wallType;
         #endregion------------------------
 
         #region ------------ Public Methods ------------
-        public void OnGameStart() => SetWallScore();
-
         public void SetWallController(WallController wallController) => this.wallController = wallController;
+
+        public void SetWallPoint() => wallPoint = wallController.GetWallPoint(wallType);
         #endregion------------------------
 
         #region ------------ Private Methods ------------
         private void OnTriggerEnter(Collider other)
         {
-            wallController.DisableWall(other.gameObject);
-        }
-
-        private void SetWallScore()
-        {
-            if (wallType == null)
-            {
-                return;
-            }
-            switch (wallType)
-            {
-                case WallType.SMALL:
-                    wallScore = wallModel.smallWallPoint;
-                    break;
-                case WallType.MID:
-                    wallScore = wallModel.midWallPoint;
-                    break;
-                case WallType.BIG:
-                    wallScore = wallModel.bigWallPoint;
-                    break;
-            }
+            wallController.DisableWall(this, other.gameObject);
         }
         #endregion------------------------
     }
