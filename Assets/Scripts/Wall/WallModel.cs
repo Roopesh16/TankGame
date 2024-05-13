6a83a4519bad09a3a00c5c0278ab584c4ddedfb1
Roopesh Verma <roopeshv.30@gmail.com>
2024-05-13 17:33:41 +0530

    Get point from model

--- Assets/Scripts/Wall/WallModel.cs
+++ Assets/Scripts/Wall/WallModel.cs
@@ -1,17 +1,25 @@
-using UnityEngine;
-namespace TankGame.Models
+namespace TankGame.Wall
 {
-    public enum WallType
+    public class WallModel
     {
-        SMALL,
-        MID,
-        BIG
-    }
+        private int smallWallPoint = 5;
+        private int midWallPoint = 10;
+        private int bigWallPoint = 15;
 
-    public class WallModel : MonoBehaviour
-    {
-        [HideInInspector] public int smallWallPoint = 5;
-        [HideInInspector] public int midWallPoint = 10;
-        [HideInInspector] public int bigWallPoint = 15;
+        private WallController wallController;
+
+        public void SetController(WallController wallController) => this.wallController = wallController;
+
+        public int GetWallPoint(WallType wallType)
+        {
+            switch (wallType)
+            {
+                case WallType.SMALL: return smallWallPoint;
+                case WallType.MID: return midWallPoint;
+                case WallType.BIG: return bigWallPoint;
+            }
+
+            return 0;
+        }
     }
 }
